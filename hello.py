a8c94f92f5fb34a67bfff61b874fbf403d675793
Evgenij <litvinov.e.v@gmail.com>
2019-07-09 09:06:29 +0300

    pre-release

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 def wsgi_app(environ, start_response):
 	start_response("200 OK", [("Content-Type", "text/plain")])
-	return environ
+	res = environ['QUERY_STRING'].replace('&', '\n')
+	return res
 
 bind = '0.0.0.0:8080'
 

444bdec6b16e7bcbcecdb65cf8ac112cfd92eb72
Evgenij <litvinov.e.v@gmail.com>
2019-07-09 09:53:36 +0300

    small fix

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 def wsgi_app(environ, start_response):
 	start_response("200 OK", [("Content-Type", "text/plain")])
-	res = environ['QUERY_STRING'].replace('&', '\n')
-	return res
+	return environ['QUERY_STRING'].replace('&', '\n')
 
 bind = '0.0.0.0:8080'
 

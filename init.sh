68ebd8f2ae23e444e7f22bf76a6e9bff6df87477
Evgenij <litvinov.e.v@gmail.com>
2019-08-12 20:10:41 +0300

    webservers conf

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo gunicorn -c hello.py hello:wsgi_app
+sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application
+
 

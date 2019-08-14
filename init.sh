1a3a1d30aecef7e35818217cf9e1ba05a6eeb917
Evgenij <litvinov.e.v@gmail.com>
2019-08-14 19:23:42 +0300

    xz

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-sudo ln -sf /home/jack/stepik/repo/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/andrtem/stepik/repo/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-#sudo gunicorn -c hello.py hello:wsgi_app
-sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application &
+sudo gunicorn -c hello.py hello:wsgi_app &
+#sudo gunicorn -c dj_gun_conf.py ask.wsgi:application &
 
 

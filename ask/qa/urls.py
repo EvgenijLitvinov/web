a3553b9131c000d35f94aec33780a53f279bba69
Litvinov <litvinov.e.v@gmail.com>
2019-08-26 09:03:44 +0300

    Django removing is done

--- ask/qa/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from qa import views
-
-urlpatterns = [
-    path('', views.test, name='test'),
-]
--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo gunicorn -c hello.py hello:wsgi_app &
-sudo gunicorn -c dj_gun_conf.py ask.wsgi:application &
+
 
 

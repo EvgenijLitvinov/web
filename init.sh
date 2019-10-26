0305062891bd41fe29f046fd1be2f08925b49afb
Evgenij <litvinov.e.v@gmail.com>
2019-10-26 19:45:46 +0300

    local good!

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/andrtem/stepik/repo/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo gunicorn -c hello.py hello:wsgi_app &
-
+cd ask
+sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application
 
 

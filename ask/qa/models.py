461887fdb6672c0c66e85bbc7ece84737b32997c
Evgenij <litvinov.e.v@gmail.com>
2020-01-12 22:14:31 +0200

    изменено:      ask/ask/settings.py 	изменено:      ask/qa/models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,10 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class QuestionManager(models.Manager):
+
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
 class Question(models.Model):
+    objects = QuestionManager()
     title = models.CharField()
     text = models.TextField()
-    added_at = models.DateField()
-    rating = models.IntegerField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.SET_NULL)
-    likes = models.ManyToManyField(User)
+    likes = models.ManyToManyField(User, related_name='likes_set')
+
+    def __str__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL)

a8e5c9c11095a0809f85ffc982b26437663ee440
Evgenij <litvinov.e.v@gmail.com>
2020-01-12 15:08:05 +0200

    изменено:      ask/qa/models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Question(models.Model):
+    title = models.CharField()
+    text = models.TextField()
+    added_at = models.DateField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User)

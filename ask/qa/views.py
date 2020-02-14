e38e8f470c1d90420aea99554a55833bacd9ad5c
Evgenij <litvinov.e.v@gmail.com>
2020-02-14 08:39:33 +0200

    working

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from django.core.paginator import Paginator
 from qa.models import Question, Answer
 from django.http import HttpResponse
 def test(request, *args, **kwargs):
 	return HttpResponse('OK')
 
 def man(request):
-	page = request.GET.get('page')
-	return HttpResponse(page)
+	questions = Question.objects.new()
+	page = request.GET.get('page', 1)
+	paginator = Paginator(questions, 10)
+	paginator.baseurl = '/?page='
+	page = paginator.page(page)
+	return render(request, 'qa/main.html', {
+		'questions': page.object_list,
+		'paginator': paginator, 'page': page
+	})
 
 def quest(request, id):
 	qu = get_object_or_404(Question, id=id)
@@ -16,4 +24,4 @@ def quest(request, id):
 		an = Answer.objects.filter(question=qu)
 	except Answer.DoesNotExist:
 		an = None
-	return render(request, 'qa/question_pattern.html', {'qu': qu, 'an': an})
+	return render(request, 'qa/question.html', {'qu': qu, 'an': an})

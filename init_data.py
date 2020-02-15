6ca95efaa8a210bfd9ed439e9296e84d581b0ebc
Evgenij <litvinov.e.v@gmail.com>
2020-02-15 08:31:22 +0200

    изменено:      init_data.py

--- init_data.py
+++ init_data.py
@@ -9,11 +9,11 @@ def rnd_str(m):
     return s
 
 # Create Users
-for _ in range(5):
-    User.objects.create_user(username=rnd_str(5))
+for i in range(1, 6):
+    User.objects.create_user(id=i, username=rnd_str(5))
 
 # Questions & Answers
-for _ in range(15):
-    Question.objects.create(title=rnd_str(5), text=rnd_str(10), rating=randint(0, 10), author=User.objects.get(id=randint(1,5)))
-    Answer.objects.create(text=rnd_str(10), author=User.objects.get(id=randint(1,5)), question_id=_)
-    Answer.objects.create(text=rnd_str(10), author=User.objects.get(id=randint(1,5)), question_id=_)
+for i in range(1, 16):
+    Question.objects.create(id=i, title=rnd_str(5), text=rnd_str(10), rating=randint(0, 10), author=User.objects.get(id=randint(1,5)))
+    Answer.objects.create(text=rnd_str(10), author=User.objects.get(id=randint(1,5)), question_id=i)
+    Answer.objects.create(text=rnd_str(10), author=User.objects.get(id=randint(1,5)), question_id=i)

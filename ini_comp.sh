c2112861ee655ae8c2f99d5c1bd610caab128018
Litvinov <litvinov.e.v@gmail.com>
2020-02-17 22:35:30 +0200

    new init files

---
+++ ini_comp.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+
+# Nginx
+sudo ln -sf /home/jack/stepik/repo/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+
+# Gunicorn
+sudo gunicorn -c hello.py hello:wsgi_app &
+cd ask
+sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application
